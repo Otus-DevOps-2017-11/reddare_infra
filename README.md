0cf456d41f34b2cf7ae6037dc6a73e21b0516075
Anton Olifir <randolph.o.webb@gmail.com>
2017-12-22 19:04:41 +0300

    corrections for HW 5 and HW 6

--- README.md
+++ README.md
@@ -41,3 +41,24 @@ gcloud compute instances create reddit-app\
 --zone=europe-west3-b \
 --metadata "startup-script-url=https://storage.googleapis.com/reddare-infra/gcp_startup.sh"
 ```
+gcloud startup script
+```
+#!/bin/sh
+
+#ruby installation
+sudo apt update
+sudo apt install -y ruby-full ruby-bundler build-essential
+
+#mongodb installation
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv EA312927
+sudo bash -c 'echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.2 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.2.list'
+sudo apt update
+sudo apt install -y mongodb-org
+sudo systemctl start mongod
+sudo systemctl enable mongod
+
+#deploy puma app
+git clone https://github.com/Otus-DevOps-2017-11/reddit.git
+cd reddit && bundle install
+puma -d
+```
--- VPN/setupvpn.sh
+++ VPN/setupvpn.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 
-multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
-echo "deb http://repo.pritunl.com/stable/apt xenial main" > 
-/etc/apt/sources.list.d/pritunl.list
+echo "deb http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" > /etc/apt/sources.list.d/mongodb-org-3.4.list
+echo "deb http://repo.pritunl.com/stable/apt xenial main" > /etc/apt/sources.list.d/pritunl.list
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 
 0C49F3730359A14518585931BC711F9BA15703C6
 apt-key adv --keyserver hkp://keyserver.ubuntu.com --recv 
@@ -12,4 +10,3 @@ apt-get --assume-yes upgrade
 apt-get --assume-yes install pritunl mongodb-org
 systemctl start pritunl mongod
 systemctl enable pritunl mongod
-
